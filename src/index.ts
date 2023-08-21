dc4ad4c8b58f95ea180fe5e00be7a32d8bdcb57f
kurakke <kcanggua@gmail.com>
2023-08-22 04:18:03 +0900

    TOKEN設定

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { webhookRouter } from './routes/webhookRouter';
 
 const app = express();
 const port = process.env.PORT || 7777;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 app.use('/webhook', webhookRouter());
