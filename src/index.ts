7b3e3ac47c71c68052eff24a226431d28ce52637
kurakke <kcanggua@gmail.com>
2023-08-23 22:22:12 +0900

    pullした

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import webhookRouter from './routes/webhook.route';
 
-import { webhookRouter } from './routes/webhookRouter';
-
 const app = express();
 const port = process.env.PORT || 7777;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
@@ -18,6 +16,6 @@ app.use(
 app.use('/webhook', webhookRouter);
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
-app.use('/webhook', webhookRouter());
+app.use('/webhook', webhookRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
