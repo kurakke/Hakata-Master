83006dab3a2c38fcf6f122737f2db3f5a1a47b3b
saezuri510 <saezuru510@gmail.com>
2023-08-24 14:14:48 +0900

    fix: lineのmiddlewareのエラーを修正しました

--- src/routes/webhook.route.ts
+++ src/routes/webhook.route.ts
@@ -1,13 +1,11 @@
-import { middleware } from '@line/bot-sdk';
 import { Router } from 'express';
 
 import { webhookController } from '../controllers/webhook.controller';
-import { middlewareConfig } from '../utils/lineConfig';
 
 export const webhookRouter = () => {
   const router = Router();
 
-  router.post('/', middleware(middlewareConfig), webhookController);
+  router.post('/', webhookController);
 
   return router;
 };
