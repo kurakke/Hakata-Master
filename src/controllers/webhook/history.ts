943af044b828e57570f35af5cb244c4449e12173
kurakke <kcanggua@gmail.com>
2023-08-25 11:20:40 +0900

    ダミーの問題作った

--- src/controllers/webhook/history.ts
+++ src/controllers/webhook/history.ts
@@ -1,7 +1,22 @@
-import { Request, Response } from 'express';
 import { lineClient } from '../../lib/line/lineClient';
 
-export const history = async (res:Response, req: Request) => {
-    const request = req.body;
-    await lineClient.replyMessage(request.replyToken, { text: request.message, type: 'text' });
+export const history = async (token:string, message:string) => {
+    await lineClient.replyMessage(token, { text: message, type: 'text' });
+    console.log(quiz.text[0]);
+}
+const quiz = {
+    answerNumber: 1,
+    choices: [
+        {
+            text: '選択肢1',
+        },
+        {
+            text: '選択肢2',
+        },
+        {
+            text: '選択肢3',
+        },
+    ],
+    number: 1,
+    text: '歴史クイズ',
 }
