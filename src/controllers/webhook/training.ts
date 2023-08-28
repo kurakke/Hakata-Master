b4ae2e267b154b35d127ec41381f97464bd13d05
saezuri510 <saezuru510@gmail.com>
2023-08-28 14:18:53 +0900

    feat: プロンプトで制御するようにしました

--- src/controllers/webhook/training.ts
+++ src/controllers/webhook/training.ts
@@ -1,6 +1,6 @@
 import { CreateChatCompletionRequestMessage } from 'openai/resources/chat';
 
-import { replaceTargets } from '../../constants/line/replaceTargets';
+// import { replaceTargets } from '../../constants/line/replaceTargets';
 import { lineClient } from '../../lib/line/lineClient';
 import { askGPT } from '../../lib/openAi/askGPT';
 import { openAiConfig } from '../../utils/openAiConfig';
@@ -9,25 +9,20 @@ import { generateTrainingFlex } from './message/generateTrainingFlex';
 
 export const training = async (text: string, replyToken: string, userId: string) => {
   if (openAiConfig.apiKey) {
-    const systems: CreateChatCompletionRequestMessage[] = [
-      {
-        content: 'ユーザーと日本語の標準語で話してください',
-        role: 'system',
-      },
-    ];
+    const systems: CreateChatCompletionRequestMessage[] = [];
 
     // TODO: チャット履歴を取得する必要がある
     const chatHistory: CreateChatCompletionRequestMessage[] = [];
 
     const messages: CreateChatCompletionRequestMessage[] = [...systems, ...chatHistory];
 
-    let reply = (await askGPT(text, messages)) || '';
+    const reply = (await askGPT(text, messages)) || '';
 
     const profile = await lineClient.getProfile(userId);
 
-    for (const replaceTarget of replaceTargets) {
-      reply = reply.replace(replaceTarget.target, replaceTarget.replace);
-    }
+    // for (const replaceTarget of replaceTargets) {
+    //   reply = reply.replace(replaceTarget.target, replaceTarget.replace);
+    // }
 
     const trainingFlex = generateTrainingFlex(reply, profile.displayName);
 
